1054a3302e3eb45f6632dd33e79a7ceda5406a21
Aman <132000890+ok-bro-calma@users.noreply.github.com>
2023-06-04 13:01:52 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,10 +1,10 @@
 { pkgs }: {
-	deps = [
-		pkgs.nodejs-19_x
+  deps = [
+    pkgs.nodejs-19_x
     pkgs.nodePackages.typescript-language-server
     pkgs.yarn
     pkgs.replitPackages.jest
     pkgs.python311
-    pkgs.ffmpeg_5-full
-	];
+    pkgs.ffmpeg
+  ];
 }
