e1845d856c0097664de389c26207c890ebedf0d9
Aman <132000890+stable-aman@users.noreply.github.com>
2023-07-04 07:59:30 +0530

    Update onboot.js

--- util/onboot.js
+++ util/onboot.js
@@ -1,35 +1,34 @@
 module.exports = (client) => {
-  // you might want to setup a dashboard, i'm currently not experienced enough for that
   const host = require('express')();
   host.get('/', (req, res) => res.send(`Status: ${client.isReady() ? 'online' : 'offline'}`));
   host.listen(2023);
 
   // add necessary listeners
-  client.on('error', (error) => {
+  client.on(client.discord.Events.Error, (error) => {
     console.log(`[client] :: error :: [${client.util.time()}]`);
     console.log(error);
   });
 
-  client.on('shardDisconnect', (event, id) => {
+  client.on(client.discord.Events.ShardDisconnect, (event, id) => {
     console.log(`[shard#${id}] :: disconnected :: [${client.util.time()}]`);
     console.log(event);
   });
 
-  client.on('shardError', (error, id) => {
+  client.on(client.discord.Events.ShardError, (error, id) => {
     console.log(`[shard#${id}] :: error :: [${client.util.time()}]`);
     console.log(error);
   });
 
-  client.on('shardReady', (id, unavailableGuilds) => {
+  client.on(client.discord.Events.ShardReady, (id, unavailableGuilds) => {
     console.log(`[shard#${id}] :: connected :: [${client.util.time()}]`);
     if (unavailableGuilds) console.log(' unavailable Guild', unavailableGuilds);
   });
 
-  client.on('shardReconnecting', (id) => {
+  client.on(client.discord.Events.ShardReconnecting, (id) => {
     console.log(`[shard#${id}] :: reconnecting :: [${client.util.time()}]`);
   });
 
-  client.on('shardResume', (id, replayedEvents) => {
+  client.on(client.discord.Events.ShardResume, (id, replayedEvents) => {
     console.log(`[shard#${id}] :: reconnected [${replayedEvents} event(s)] :: [${client.util.time()}]`);
   });
 
