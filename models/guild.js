3e37c817d93f9df51111e05d1538914b2f591143
Aman <132000890+stable-aman@users.noreply.github.com>
2023-07-04 08:01:34 +0530

    Update guild.js

--- models/guild.js
+++ models/guild.js
@@ -4,6 +4,7 @@ const { prefix } = require('../util/config.js');
 // new to mongodb? its easy don't worry
 // get started at https://mongoosejs.com/docs/index.html
 module.exports = ({ model, Schema }) => model('guild', Schema({
+  // you can add more properties here
   id: String,
   prefix: { type: String, default: prefix }
 }));
